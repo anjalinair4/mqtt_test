06d36117f7793489c476e9030135711883d65b73
anjalinair4 <nairanjali004@gmail.com>
2015-12-14 13:14:13 +0530

    task21

--- task2/task2.ino
+++ task2/task2.ino
@@ -1,6 +1,8 @@
 
 #include<EEPROM.h>
 #include <ESP8266WiFi.h>
+//#include <SPI.h>
+//#include <WiFi.h>
 
 const int Pin = 2;
 void setup() {
@@ -10,7 +12,7 @@ pinMode(Pin, INPUT);
 EEPROM.begin(9600);
 
 WiFiServer Server(80);
-//read ssid and password from eeprom
+
 if(Pin==LOW)
 {
   for(i=0;i<10;i++)
@@ -23,14 +25,23 @@ if(Pin==LOW)
   }
   WiFi.begin(ssid, pass);
 }
-// starting WiFiWebServer
 else 
 {
  Server.begin();
  Server.print("hello world"); 
+ WiFiClient Client=Server.available();
+if(!Client)
+{
+ delay(1); 
+}
+while(!Client.available())
+{
+  delay(1);
+}
+}
 }
 
-}
+
 
 void loop() {
   // put your main code here, to run repeatedly:
